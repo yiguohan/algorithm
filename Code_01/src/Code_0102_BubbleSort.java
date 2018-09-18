451279ec82d0fd645412f95f395a1fc34e0d6f62
yiguohan <yiguohan@gmail.com>
2018-09-18 15:17:25 +0800

    Bubble sort complete

---
+++ Code_01/src/Code_0102_BubbleSort.java
@@ -0,0 +1,41 @@
+/**
+ * 冒泡排序
+ */
+public class Code_0102_BubbleSort {
+
+    public static void main(String args[]) {
+        int[] testArr = new int[]{2, 3, 1, 4, 5, 3, 2, 1, 5, 2, 45, 5, 66};
+        bubbleSort(testArr);
+        for (int i :
+                testArr) {
+            System.out.println(i);
+        }
+    }
+
+    private static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = arr.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * 交换位置的方法
+     *
+     * @param arr
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int k = arr[i];
+        arr[j] = arr[i];
+        arr[i] = k;
+    }
+}
