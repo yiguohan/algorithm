b5a20d945f6bec32b61480671ed8217dd5cb3033
yiguohan <yiguohan@gmail.com>
2018-09-20 14:26:20 +0800

    添加比较器 修改了Selection的一些bug

--- Code_01/src/Code_0102_BubbleSort.java
+++ Code_01/src/Code_0102_BubbleSort.java
@@ -1,16 +1,9 @@
+import java.util.Arrays;
+
 /**
  * 冒泡排序
  */
 public class Code_0102_BubbleSort {
-
-    public static void main(String args[]) {
-        int[] testArr = new int[]{2, 3, 1, 4, 5, 3, 2, 1, 5, 2, 45, 5, 66};
-        bubbleSort(testArr);
-        for (int i :
-                testArr) {
-            System.out.println(i);
-        }
-    }
 
     private static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
@@ -37,5 +30,86 @@ public class Code_0102_BubbleSort {
         int k = arr[i];
         arr[i] = arr[j];
         arr[j] = k;
+    }
+
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            System.out.println("数组的长度不一致！请确认！");
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                System.out.println("结果不一致，在索引为 " + i + " 的数据发生第一次错误！正确的排序如下：");
+                printArray(arr2);
+                System.out.println("错误的排序如下：");
+                printArray(arr1);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
     }
 }
