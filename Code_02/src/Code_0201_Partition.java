e98f370a0d0619ee327f89cb8bc5374a38561959
yiguohan <yiguohan@gmail.com>
2018-09-28 17:00:31 +0800

    Code Partition Method

--- Code_02/src/Code_0201_Partition.java
+++ Code_02/src/Code_0201_Partition.java
@@ -2,4 +2,27 @@
  * Partition函数
  */
 public class Code_0201_Partition {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void partition(int[] arr, int target) {
+
+        int i = -1;//小于等于区的右边界
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] < target) {
+                swap(arr, j, ++i);
+                i++;
+            }
+        }
+
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int k = arr[a];
+        arr[a] = arr[b];
+        arr[b] = k;
+    }
 }
