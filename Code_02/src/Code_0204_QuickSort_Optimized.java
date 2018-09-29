dae42790eff4c15ae6a7242c688a2792bf0212cd
yiguohan <yiguohan@gmail.com>
2018-09-29 15:26:44 +0800

    QuickSort_Optimized Complete

--- Code_02/src/Code_0204_QuickSort_Optimized.java
+++ Code_02/src/Code_0204_QuickSort_Optimized.java
@@ -1,7 +1,128 @@
+import java.util.Arrays;
+
 /**
  * 优化快速排序
  * 时间复杂度O(N*logN)
  * 额外空间复杂度O(logN)
  */
 public class Code_0204_QuickSort_Optimized {
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            quickSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+    private static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
+            int[] p = partition(arr, l, r);
+            quickSort(arr, l, p[0] - 1);
+            quickSort(arr, p[1] + 1, r);
+        }
+    }
+
+    private static int[] partition(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        int i = l;
+        while (i < more) {
+            if (arr[i] < arr[r]) {
+                swap(arr, i++, ++less);
+            } else if (arr[i] > arr[r]) {
+                swap(arr, i, --more);
+            } else {
+                i++;
+            }
+        }
+        swap(arr, more, r);
+        return new int[]{less + 1, more};
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            System.out.println("数组的长度不一致！请确认！");
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                System.out.println("结果不一致，在索引为 " + i + " 的数据发生第一次错误！正确的排序如下：");
+                printArray(arr2);
+                System.out.println("错误的排序如下：");
+                printArray(arr1);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
