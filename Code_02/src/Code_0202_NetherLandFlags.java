889289b46e996309a93e9820f7dfa69182dd8190
yiguohan <yiguohan@gmail.com>
2018-09-29 10:47:44 +0800

    NetherLandFlags Complete

--- Code_02/src/Code_0202_NetherLandFlags.java
+++ Code_02/src/Code_0202_NetherLandFlags.java
@@ -4,12 +4,18 @@
 public class Code_0202_NetherLandFlags {
 
     public static void main(String[] args) {
+        int[] test = generateArray();
 
+        printArray(test);
+        int[] res = partition(test, 0, test.length - 1, 1);
+        printArray(test);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
     }
 
-    private static void partition(int[] arr, int target) {
-        int less = -1;
-        int more = arr.length;
+    private static int[] partition(int[] arr, int l, int r, int target) {
+        int less = l - 1;
+        int more = r + 1;
         int i = 0;
         while (i < more) {
             if (arr[i] > target) {
@@ -23,11 +29,32 @@ public class Code_0202_NetherLandFlags {
                 i++;
             }
         }
+        return new int[]{less + 1, more - 1};
     }
 
     private static void swap(int[] arr, int a, int b) {
         int k = arr[a];
         arr[a] = arr[b];
         arr[b] = k;
+    }
+
+    // for test
+    public static int[] generateArray() {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 3);
+        }
+        return arr;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
