1e941e3c7aa00c66fede04724e7ba9be46909b94
yiguohan <yiguohan@gmail.com>
2019-03-14 09:22:18 +0800

    InsertionSort Renew

--- Code_01/src/Code_0104_InsertionSort.java
+++ Code_01/src/Code_0104_InsertionSort.java
@@ -11,10 +11,17 @@ public class Code_0104_InsertionSort {
             return;
         }
 
-        for (int i = 1; i <= arr.length - 1; i++) {
-            for (int j = i; j - 1 >= 0; j--) {
+//        for (int i = 1; i <= arr.length - 1; i++) {
+//            for (int j = i; j - 1 >= 0; j--) {
+//                if (arr[j] < arr[j - 1]) {
+//                    swap(arr, j, j - 1);
+//                }
+//            }
+//        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
-                    swap(arr, j, j - 1);
+                    swap(arr, j - 1, j);
                 }
             }
         }
