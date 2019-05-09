4dff2b5c2b628c67de503363f27ae993fdd226ae
yiguohan <yiguohan@gmail.com>
2019-05-09 16:15:17 +0800

    Add ReverseList

--- Code_05/src/Code_0501_ReverseList.java
+++ Code_05/src/Code_0501_ReverseList.java
@@ -6,4 +6,51 @@
  * * 额外空间复杂度O(1)
  */
 public class Code_0501_ReverseList {
+
+    //-------------------------------------反转单链表--------------------------------------//
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node reverseList(Node head) {
+        Node next = null;
+        Node pre = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    //-------------------------------------反转双向链表--------------------------------------//
+    public static class DoubleNode {
+        int val;
+        DoubleNode next;
+        DoubleNode pre;
+
+        DoubleNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private void reverseDoubleList(DoubleNode head) {
+        DoubleNode next = null;
+        DoubleNode pre = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.pre = next;
+            pre = head;
+            head = next;
+        }
+    }
+
 }
